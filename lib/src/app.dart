55ee2d9ecfb8398a0021360461a4794390e42c7d
BorisGautier <tchoukouahaboris@outlook.fr>
2021-09-18 09:40:01 +0100

    support multiple languages & add flutter_theme_x

---
+++ lib/src/app.dart
@@ -0,0 +1,84 @@
+import 'package:aesthetic_dialogs/aesthetic_dialogs.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: AppLocalizations.of(context)!.appname,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      locale: Locale('fr'),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              ElevatedButton(
+                onPressed: buildDialog,
+                child: Text("Open Dialog"),
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: Icon(Icons.add),
+        ));
+  }
+
+  Future<void> buildDialog() async {
+    AestheticDialogs.showDialog(
+        title: "My Dialog",
+        message: "Hello!!!",
+        cancelable: true,
+        darkMode: false,
+        dialogAnimation: DialogAnimation.IN_OUT,
+        dialogGravity: DialogGravity.CENTER,
+        dialogStyle: DialogStyle.FLASH,
+        dialogType: DialogType.SUCCESS,
+        duration: 3000);
+  }
+}
