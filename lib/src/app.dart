1d2a8557a1f79cc1899bdd109297b6a5cf171d0b
BorisGautier <tchoukouahaboris@outlook.fr>
2021-09-18 09:42:05 +0100

    fluuter format code

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,5 @@
 import 'package:aesthetic_dialogs/aesthetic_dialogs.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class MyApp extends StatelessWidget {
