9490039e37260c2953364d90311b30c1889da4fe
BorisGautier <tchoukouahaboris@outlook.fr>
2021-09-20 16:02:09 +0100

    Update config project

--- lib/src/api/appwrite.dart
+++ lib/src/api/appwrite.dart
@@ -1,5 +1,5 @@
 import 'package:appwrite/appwrite.dart';
-import 'package:flutter_appwrite/src/utils/strings.dart';
+import 'package:flutter_appwrite/src/utils/config.dart';
 
 Account initAppWrite() {
   Client client = Client();
--- lib/src/utils/strings.dart
+++
@@ -1,3 +0,0 @@
-const appWriteApiUrl = "https://appwrite.tbg.cm/v1";
-const appwriteProjectId = "6112897166747";
-const appWriteUrl = "https://appwrite.tbg.cm";
